9ca4eac8feb443dda7cc8ea51333181a876daf73
dingjie27 <48076430+dingjie27@users.noreply.github.com>
2019-07-29 09:45:04 +0800

    Update EurekaApplicationTests.java

--- src/test/java/eureka/EurekaApplicationTests.java
+++ src/test/java/eureka/EurekaApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaApplicationTests {
-
+//test application
 	@Test
 	public void contextLoads() {
 	}
